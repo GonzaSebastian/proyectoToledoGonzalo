48281663fea5789c7cd70497dd474d0af60f7ab8
Gonzalo Toledo <gonzalo.stoledo@gmail.com>
2023-06-06 19:57:01 -0300

    fix: add status(200)

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -5,23 +5,23 @@ const cartsRouter = Router()
 const carts = new cartManager("./src/models/carts.json")
 
 cartsRouter.get("/", async (req, res) => {
-  res.send(await carts.getCart())
+  res.status(200).send(await carts.getCart())
 })
 
 cartsRouter.get("/:id", async (req, res) => {
   let id = req.params.id
-  res.send(await carts.getCartById(id))
+  res.status(200).send(await carts.getCartById(id))
 })
 
 cartsRouter.post("/", async (req, res) => {
-  res.send(await carts.addCart())
+  res.status(200).send(await carts.addCart())
 })
 
 cartsRouter.post("/:cid/products/:pid", async (req, res) => {
   let cartId = req.params.cid
   let productId = req.params.pid
 
-  res.send(await carts.addProductInCart(cartId, productId))
+  res.status(200).send(await carts.addProductInCart(cartId, productId))
 })
 
 export default cartsRouter
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -13,23 +13,23 @@ productRouter.post("/", async (req, res) => {
 
 productRouter.get("/", async (req, res) => {
   const getProducts = await product.getProducts()
-  res.send(getProducts)
+  res.status(200).send(getProducts)
 })
 
 productRouter.get("/:id", async (req, res) => {
   let id = req.params.id
-  res.send(await product.getProductById(id))
+  res.status(200).send(await product.getProductById(id))
 })
 
 productRouter.put("/:id", async (req, res) => {
   let id = req.params.id
   let update = req.body
-  res.send(await product.updateProduct(id, update))
+  res.status(200).send(await product.updateProduct(id, update))
 })
 
 productRouter.delete("/:id", async (req, res) => {
   let id = req.params.id
-  res.send(await product.deleteProduct(id))
+  res.status(200).send(await product.deleteProduct(id))
 })
 
 export default productRouter
