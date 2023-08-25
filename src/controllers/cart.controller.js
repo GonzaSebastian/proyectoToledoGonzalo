013a8654a48d2b15b051c1824c096c9265af442e
Gonzalo Toledo <gonzalo.stoledo@gmail.com>
2023-08-25 00:13:36 -0300

    fix: cart.controller correction

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,62 +1,100 @@
 import { cartModel } from "../dao/models/cart.model.js"
 
-  export const getCart = async() => {
-    const carts = await cartModel.find()
-    return carts;
+  export const getCart = async (req, res) => {
+    try {
+      const carts = await cartModel.find()
+      res.status(200).json({ status: 'success', payload: carts })
+    } catch(err) {
+      res.status(404).json({ status: 'error', error: err.message })
+    }
   }
 
   export const getCartById = async(cartId) => {
   const cart = await cartModel.findOne({_id:cartId}).lean().populate('products.product')
-    return cart;;
-  }
-
-  export const addCart = async() => {
-     await cartModel.create({product:[]})
-  }
-
-  export const addProductInCart = async (cartId, productId) => {
-
-    const cart = await cartModel.findOne({_id: cartId})
-    const productIndex = cart.products.findIndex(
-      (item) => item.product.toString() == productId
-    );
-
-    if (productIndex !== -1) {
-      cart.products[productIndex].quantity++;
-    } else {
-      cart.products.push({ product: productId, quantity: 1 });
-    }
-    await cartModel.updateOne({_id: cartId}, cart)
-
-
     return cart;
   }
 
-  export const deleteProductInCart = async (cartId, productId) => {
-    const cart = await cartModel.findOne({_id: cartId})
-    const productsFilter = cart.products.findIndex(i => i.product.toString() == productId)
-    if (cart && productsFilter !== -1) {
-      cart.products.splice(productsFilter, 1)
-      await cartModel.updateOne({_id: cartId}, cart)
-      return cart
-    } else {
-      return "Cart or product not found"
+  export const addCart = async (req, res) => {
+    try {
+      await cartModel.create({product:[]})
+      res.status(200).json({ status: 'success', mesagge: 'Cart created successfully' })
+    } catch(err) {
+      res.status(404).json({ status: 'error', error: err.message })
     }
   }
 
-  export const cleanCart = async (cartId) => {
-    const cart = await cartModel.findOne({_id: cartId})
-    cart.products.splice([])
-    await cartModel.updateOne({_id: cartId}, cart)
-    return cart
+  export const addProductInCart = async (req, res) => {
+    let cartId = req.params.cid
+    let productId = req.params.pid
+    try {
+      const cart = await cartModel.findOne({_id: cartId})
+
+      const productIndex = cart.products.findIndex(
+        (item) => item.product.toString() == productId
+      );
+
+      if (productIndex !== -1) {
+        cart.products[productIndex].quantity++;
+      } else {
+        cart.products.push({ product: productId, quantity: 1 });
+      }
+      await cartModel.updateOne({_id: cartId}, cart)
+
+      res.status(200).json({ status: 'success', payload: cart})
+    } catch(err) {
+      res.status(404).json({ status: 'error', error: err.message })
+    }
+  
   }
 
-  export const updateQuantityProduct = async (cartId, productId, newQuantity) => {
-    const cart = await cartModel.findOne({_id: cartId})
-    if (!cart) return console.error(err.message)
-    const productIndex = cart.products.findIndex(i => i.product.toString() == productId)
+  export const deleteProductInCart = async (req, res) => {
+    let cartId = req.params.cid
+    let productId = req.params.pid
+    try {
+      const cart = await cartModel.findOne({_id: cartId})
+      const productsFilter = cart.products.findIndex(i => i.product.toString() == productId)
+      if (cart && productsFilter !== -1) {
+        cart.products.splice(productsFilter, 1)
+        await cartModel.updateOne({_id: cartId}, cart)
+      } else {
+        return "Cart or product not found"
+      }
 
-    cart.products[productIndex].quantity = newQuantity 
-    await cartModel.updateOne({_id: cartId}, cart)
-    return cart
+      res.status(200).json({status: 'success', payload: cart})
+    } catch(err) {
+      res.status(404).json({ status: 'error', error: err.message })
+    }
+  }
+
+  export const cleanCart = async (req, res) => {
+    let cartId = req.params.cid
+    try {
+      const cart = await cartModel.findOne({_id: cartId})
+      cart.products.splice([])
+      await cartModel.updateOne({_id: cartId}, cart)
+
+      res.status(200).json({status: 'success', payload: cart})
+    } catch(err) {
+      res.status(404).json({ status: 'error', error: err.message })
+    }
+  }
+
+  export const updateQuantityProduct = async (req, res) => {
+    let cartId = req.params.cid
+    let productId = req.params.pid
+    let newQuantity = req.body.quantity
+
+    try {
+      const cart = await cartModel.findOne({_id: cartId})
+      if (!cart) return console.error(err.message)
+      const productIndex = cart.products.findIndex(i => i.product.toString() == productId)
+
+      cart.products[productIndex].quantity = newQuantity 
+      await cartModel.updateOne({_id: cartId}, cart)
+
+      res.status(200).json({status: 'success', payload: cart})
+    } catch(err) {
+      res.status(404).json({status: 'error', error: err.message})
+    }
+  
   }
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -3,55 +3,16 @@ import {getCart, addCart, addProductInCart, deleteProductInCart, cleanCart, upda
 
 const cartsRouter = Router()
 
-cartsRouter.get("/", async (req, res) => {
-  try {
-    res.status(200).json({ status: 'success', payload: await getCart() })
-  } catch(err) {
-    res.status(404).json({ status: 'error', error: err.message })
-  }
-})
+cartsRouter.get("/", getCart)
 
-cartsRouter.post("/", async (req, res) => {
-  try {
-    await addCart()
-    res.status(200).json({ status: 'success', mesagge: 'Cart created successfully' })
-  } catch(err) {
-    res.status(404).json({ status: 'error', error: err.message })
-  }
-})
+cartsRouter.post("/", addCart)
 
-cartsRouter.post("/:cid/products/:pid", async (req, res) => {
-  let cartId = req.params.cid
-  let productId = req.params.pid
+cartsRouter.post("/:cid/products/:pid", addProductInCart)
 
-  res.status(200).json(await addProductInCart(cartId, productId))
-})
+cartsRouter.delete("/:cid/products/:pid", deleteProductInCart)
 
-cartsRouter.delete("/:cid/products/:pid", async (req, res) => {
-  let cartId = req.params.cid
-  let productId = req.params.pid
+cartsRouter.delete("/:cid", cleanCart)
 
-  res.status(200).json(await deleteProductInCart(cartId, productId))
-})
-
-cartsRouter.delete("/:cid", async (req, res) => {
-  let cartId = req.params.cid
-
-  res.status(200).json(await cleanCart(cartId))
-})
-
-cartsRouter.put("/:cid/products/:pid", async (req, res) => {
-  try {
-    let cartId = req.params.cid
-    let productId = req.params.pid
-    let newQuantity = req.body.quantity
-    const productNewQuant = await updateQuantityProduct(cartId, productId, newQuantity)
-
-    res.status(200).json({status: 'success', payload: productNewQuant})
-  } catch(err) {
-    res.status(404).json({status: 'error', error: err.message})
-  }
-
-})
+cartsRouter.put("/:cid/products/:pid", updateQuantityProduct)
 
 export default cartsRouter
